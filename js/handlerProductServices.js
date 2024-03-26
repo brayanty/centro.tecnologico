383af779761dd1303e1f16fe9e33f240c7c38942
brayanty <brayanty>
2024-03-25 22:34:21 -0500

    add function

--- js/handlerProductServices.js
+++ js/handlerProductServices.js
@@ -9,25 +9,37 @@ productsServices.map((PS) => {
 
         switch (PStext) {
             case "Cambios de pantallas":
-                linkClick(linkWhatsapp + 'Buenas, deseo obtener un cambio de pantalla a mi celular');
+                if(confirm('¿Quieres salir del sitio?',)){
+                    linkClick(linkWhatsapp + 'Buenas, deseo obtener un cambio de pantalla a mi celular');
+                }
                 break;
             case "Cambio de altavoces":
-                linkClick(linkWhatsapp + 'Buenas, deseo cambiar el altavoz de mi celular');
+                if(confirm('¿Quieres salir del sitio?',)){
+                    linkClick(linkWhatsapp + 'Buenas, deseo cambiar el altavoz de mi celular');
+                }
                 break;
             case "Reparación de fallas electronícas":
-                linkClick(linkWhatsapp + 'Buenas, deseo obtener una reparación para mi dispositivo');
+                if(confirm('¿Quieres salir del sitio?',)){
+                    linkClick(linkWhatsapp + 'Buenas, deseo obtener una reparación para mi dispositivo');
+                }
                 break;
 
             case "Venta de acesorios":
-                linkClick(linkWhatsapp + 'Buenas, deseo obtener una lista de los acesorios disponibles');
+                if(confirm('¿Quieres salir del sitio?',)){
+                    linkClick(linkWhatsapp + 'Buenas, deseo obtener una lista de los acesorios disponibles');
+                }
                 break;
 
             case "Protectores de pantalla y forros":
-                linkClick(linkWhatsapp + 'Buenas, deseo obtener información de protectores o forros');
+                if(confirm('¿Quieres salir del sitio?',)){
+                    linkClick(linkWhatsapp + 'Buenas, deseo obtener información de protectores o forros');
+                }
                 break;
 
             case "Reparación de Equipos de sonidos":
-                linkClick(linkWhatsapp + 'Buenas, deseo obtener información de un dispositivo de sonido');
+                if(confirm('¿Quieres salir del sitio?',)){
+                    linkClick(linkWhatsapp + 'Buenas, deseo obtener información de un dispositivo de sonido');
+                }
                 break;
 
             default:
@@ -36,3 +48,4 @@ productsServices.map((PS) => {
         }
     });
 });
+4194304
